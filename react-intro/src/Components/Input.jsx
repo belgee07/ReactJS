b32528b6da5abe87ac43933cf7e3e17ec9b0e00c
belgee7 <belge0722@gmail.com>
2024-08-15 12:33:18 +0800

    delete

--- react-intro/src/Components/Input.jsx
+++ react-intro/src/Components/Input.jsx
@@ -1,15 +1,11 @@
 export const Input = ({ placeholder, value, onChange }) => {
-  const handleChange = (event) => {
-    onChange(event.target.value);
-  };
-
   return (
     <div>
       <input
         className="input"
         placeholder={placeholder}
         value={value}
-        onChange={handleChange}
+        onChange={onChange}
       />
     </div>
   );
--- react-intro/src/pages/index.js
+++ react-intro/src/pages/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Button } from "@/Components/Button";
 import { Input } from "@/Components/Input";
+import { FaTrash } from "react-icons/fa6";
+import { MdOutlineModeEdit } from "react-icons/md";
 
 export default function Home() {
   const [text, setText] = useState("");
@@ -13,17 +15,31 @@ export default function Home() {
     }
   };
 
+  const handleIconClick = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="todo">
       <h1>ToDo List</h1>
       <div className="check">
-        <Input placeholder="New Task" value={text} onChange={setText} />
+        <Input
+          placeholder="New Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <Button onClick={handleAddTask} />
       </div>
 
-      <div className="task-list">
+      <div className="task-lists">
         {tasks.map((task, index) => (
-          <h3 key={index}>{task}</h3>
+          <div className="list" key={index}>
+            {task}
+            <div className="rightSides">
+              <MdOutlineModeEdit />
+              <FaTrash onClick={() => handleIconClick(index)} />
+            </div>
+          </div>
         ))}
       </div>
     </div>
